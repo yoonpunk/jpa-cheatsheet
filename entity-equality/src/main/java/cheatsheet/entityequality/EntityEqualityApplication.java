90c416236ecf4deb076ebbaf7c84053434ef9e48
yoonpunk <yoonys427@outlook.com>
2023-03-14 22:39:49 +0900

    init entity-equality

---
+++ entity-equality/src/main/java/cheatsheet/entityequality/EntityEqualityApplication.java
@@ -0,0 +1,13 @@
+package cheatsheet.entityequality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityEqualityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntityEqualityApplication.class, args);
+    }
+
+}
